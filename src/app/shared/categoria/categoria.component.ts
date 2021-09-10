cf690e9bd70fee3b2686669a1af838b3609e8a7d
Arcaino <claudio.dzuwa@gmail.com>
2021-09-10 16:47:44 -0300

    feat(categoria): ajustes nas medidas do slider

--- src/app/shared/categoria/categoria.component.ts
+++ src/app/shared/categoria/categoria.component.ts
@@ -14,14 +14,44 @@ import 'swiper/css/pagination';
 export class CategoriaComponent implements OnInit {
 
   config: SwiperOptions = {
-    spaceBetween: 60,
+    spaceBetween: 200,
     initialSlide: 1,
     autoHeight: false,
     direction: 'horizontal',
     loop: true,
     slidesPerView: 2,
     centeredSlides: true,
-    slidesOffsetBefore: 0,
+    slidesOffsetBefore: -90,
+    breakpoints: {
+      360: {
+        spaceBetween: 230,
+        slidesOffsetBefore: -100,
+      },
+      420: {
+        spaceBetween: 270,
+        slidesOffsetBefore: -120,
+      },
+      500: {
+        spaceBetween: 300,
+        slidesOffsetBefore: -130,
+      },
+      570: {
+        spaceBetween: 380,
+        slidesOffsetBefore: -165,
+      },
+      700: {
+        spaceBetween: 480,
+        slidesOffsetBefore: -205,
+      },
+      900: {
+        spaceBetween: 580,
+        slidesOffsetBefore: -255,
+      },
+      990: {
+        spaceBetween: 1200,
+        slidesOffsetBefore: -550,
+      }
+    }
   };
 
   filmesPopulares: Array<any> = new Array();
